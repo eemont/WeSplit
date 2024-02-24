f290d2928203f17d67dbf3093107d72fbd392de8
Emmanuel Montoya <emmanuelmontoya02@gmail.com>
2024-02-24 12:17:25 -0800

    Uploaded completed files

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,71 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Emmanuel Montoya on 2/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [5, 10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+
+    }
+
+    var body: some View {
+        NavigationStack {
+            Form {
+                /// Text Field - Insert amount
+                Section("Check Total"){
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Text("How much tip do you want to leave?")
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    /// Amount of people the check is going to be divided by
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section("Check Total With Tip"){
+                        Text(checkAmount + (checkAmount * Double(tipPercentage) / 100), format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+                /// Results - Translate - Amount
+                Section("Total Per Person"){
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
